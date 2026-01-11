b86506c0b447e57998f5d2fe82504306a84caafc
skmumit11 <sk11mumit@gmail.com>
2026-01-11 22:12:02 +0600

    Refreshing dev branch

--- webtechProject1_5/views/ambulanceAccept.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-require_once  '../models/ambulanceModel.php';
-$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
-if ($id > 0) { updateAmbulanceStatus($id, 'Accepted'); }
-header('Location: admindashboard.php');
--- webtechProject1_5/views/ambulanceDelete.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-require_once ('../models/ambulanceModel.php');
-$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
-if ($id > 0) { deleteAmbulanceRequest($id); }
-header('Location: admindashboard.php');
