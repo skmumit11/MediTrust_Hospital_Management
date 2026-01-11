6620678aa49a367885e168a7b1b92f5339f37da1
SK Mumitur Rahman <sk11mumit@gmail.com>
2026-01-11 22:16:53 +0600

    Delete webtechProject1_5 directory

--- webtechProject1_5/views/bedRelease.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-require_once ('../models/bedModel.php');
-$allocId = isset($_GET['id']) ? (int)$_GET['id'] : 0;
-if ($allocId > 0) { releaseBedAllocation($allocId); }
-header('Location: bedAllocationList.php');
-
