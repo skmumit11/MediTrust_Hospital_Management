5347bfa23659e68ed931f176fc2e8bc5734096da
skmumit11 <sk11mumit@gmail.com>
2026-01-20 01:38:57 +0600

    index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+header('location: webtechProject1_5/index1_5.php');
+
+// header('/webtechProject6_10/index6_10.php');
+// header('/webtechProject11_15/index11_15.php');
+// header('/webtechProject16_20/index16_20.php');
+
+
+
+?>
